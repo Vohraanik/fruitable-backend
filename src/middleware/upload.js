4122974883363fc1737ca36ab1d357ba5f60f4f7
Vohraanik <anikvohra3@gmail.com>
2024-08-22 08:48:34 +0530

    s

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -3,24 +3,23 @@ const fs = require('fs');
 const path = require('path');
 
 const storage = multer.diskStorage({
-
   destination: function (req, file, cb) {
-    const filedname = file.fieldname
-    const pathUpload = path.join("public", filedname);
+    const fieldname = file.fieldname;
+    const pathUpload = path.join("public", fieldname);
 
-    fs.mkdir(pathUpload,{recursive:true},(err)=>{
-      if(err){
-        cb(err,null)
-        }
-    })
-    cb(null, pathUpload);
+    fs.mkdir(pathUpload, { recursive: true }, (err) => {
+      if (err) {
+        return cb(err, null);
+      }
+      cb(null, pathUpload); // Move this inside the fs.mkdir callback
+    });
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniqueSuffix + '-' + file.originalname)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
   }
-})
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 module.exports = upload;
