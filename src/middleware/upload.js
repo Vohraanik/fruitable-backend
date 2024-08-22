1d95e4e38e8d51d6cbf09a73d1c0e39ee1211978
Vohraanik <anikvohra3@gmail.com>
2024-08-22 10:37:10 +0530

    dfv

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -5,9 +5,9 @@ const path = require('path');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const fieldname = file.fieldname;
-    // const pathUpload = path.resolve('./public', fieldname);
+    const pathUpload = path.resolve('../../public/image', fieldname);
 
-    const pathUpload = path.join("/tmp", fieldname);
+    // const pathUpload = path.join("/tmp", fieldname);
 
   //   const pathUpload = path.join(
   //     process.cwd(),
