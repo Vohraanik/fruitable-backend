c808c215650adccf4089e921ef6c82f81a445b74
Vohraanik <anikvohra3@gmail.com>
2024-08-22 11:18:23 +0530

    d

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,30 +1,64 @@
+// const multer = require('multer');
+// const fs = require('fs');
+// const path = require('path');
+
+
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const fieldname = file.fieldname;
+//     const pathUpload = path.join('public',fieldname);
+
+//     // const pathUpload = path.join("/tmp", fieldname);
+
+//   //   const pathUpload = path.join(
+//   //     process.cwd(),
+//   //     fieldname
+//   // );
+
+//     console.log(pathUpload,fieldname,);
+    
+
+//     fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
+//       if (err) {
+//         return cb(err, null);
+//       }
+//       cb(null, pathUpload); 
+//     });
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, uniqueSuffix + '-' + file.originalname);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-
+// Check if the code is running in Vercel environment
+const isVercel = process.env.VERCEL || process.env.NOW_REGION;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const fieldname = file.fieldname;
-    const pathUpload = path.join('public',fieldname);
 
-    // const pathUpload = path.join("/tmp", fieldname);
+    // Use '/tmp' directory on Vercel, otherwise use 'public' directory locally
+    const pathUpload = isVercel 
+      ? path.join('/tmp', fieldname)
+      : path.join('public', fieldname);
 
-  //   const pathUpload = path.join(
-  //     process.cwd(),
-  //     fieldname
-  // );
+    // Log the upload path
+    console.log(`Uploading to: ${pathUpload}`);
 
-    console.log(pathUpload,fieldname,);
-    
+    // Create directory if it doesn't exist
+    fs.mkdirSync(pathUpload, { recursive: true });
 
-    fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
-      if (err) {
-        return cb(err, null);
-      }
-      cb(null, pathUpload); 
-    });
+    cb(null, pathUpload); 
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -35,3 +69,4 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 module.exports = upload;
+
