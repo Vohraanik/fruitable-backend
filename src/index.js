c631fef4fce67fce629389b9aa38f3d21d3d18e3
Vohraanik <anikvohra3@gmail.com>
2024-08-22 11:34:33 +0530

    gg

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use(
     }
   })
 );
-
+app.use(express.static(path.resolve('./public')));
 // const swaggerDocument = YAML.load('./src/api.yaml'); 
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors(
